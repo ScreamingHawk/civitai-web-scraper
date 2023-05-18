a5810b692f9c04805c52655f811d673685ab76b8
Michael Standen <screaminghawk@gmail.com>
2023-05-19 07:13:36 +1200

    Server side pagination

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import os
 import json
 import random
 import configparser
+from urllib.parse import urlparse, parse_qs
 from jinja2 import Environment, FileSystemLoader
 
 # Configuration
@@ -11,6 +12,7 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 output_dir = config["DEFAULT"]["OutputDir"]
 PORT = int(config["DEFAULT"]["ServerPort"])
+ITEMS_PER_PAGE = int(config["DEFAULT"]["ItemsPerPage"])
 local_images = config["DEFAULT"]["LocalImages"] == 'True'
 
 data = []
@@ -43,27 +45,56 @@ def load_image_data():
 # Create a custom handler for HTTP requests
 class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/':
-            global data
+        global data
 
+        url_parts = urlparse(self.path)
+        path = url_parts.path
+        query = parse_qs(url_parts.query)
+        search_term = query.get('search', [''])[0]  # Get search term from query params
+
+        if path == '/':
+            # Write the HTML page
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
 
             env = Environment(loader=FileSystemLoader('.'))
             template = env.get_template('web/template.html')
-            output = template.render(data=data)
+            output = template.render()
             self.wfile.write(output.encode('utf-8'))
-        elif self.path.startswith(f"/{output_dir}/") or self.path.startswith('/web/'):
+
+        elif path.startswith(f"/{output_dir}/") or path.startswith('/web/'):
+            # Write other static files
             self.send_response(200)
-            if self.path.endswith('.json'):
+            if path.endswith('.json'):
                 self.send_header('Content-type', 'application/json')
-            elif self.path.endswith('.css'):
+            elif path.endswith('.css'):
                 self.send_header('Content-type', 'text/css')
             self.end_headers()
-            with open('.' + self.path, 'rb') as f:
+            with open('.' + path, 'rb') as f:
                 self.wfile.write(f.read())
+
+        elif path.startswith("/page/"):
+            # Pagination
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+
+            page_number = int(path.split("/")[2])
+            start_index = ITEMS_PER_PAGE * (page_number - 1)
+            end_index = start_index + ITEMS_PER_PAGE
+
+            if search_term:
+                # Filter data based on search term
+                filtered_data = [item for item in data if search_term.lower() in json.dumps(item['data']).lower()]
+                page_data = filtered_data[start_index:end_index]
+            else:
+                page_data = data[start_index:end_index]
+
+            self.wfile.write(json.dumps(page_data).encode('utf-8'))
+
         else:
+            # 404 probably
             super().do_GET()
 
 load_image_data()
